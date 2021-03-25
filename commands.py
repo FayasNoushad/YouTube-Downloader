6ee222bb2d48be40eea2b31b647a8a6e450f33b2
Fayas <76828314+FayasNoushad@users.noreply.github.com>
2021-03-25 15:38:31 +0530

    Update commands.py

--- commands.py
+++ commands.py
@@ -3,8 +3,7 @@ if bool(os.environ.get("WEBHOOK", False)):
     from sample_config import Config
 else:
     from config import Config
-from translation import Translation 
-from buttons import Button
+from translation import Translation
 from pyrogram import Client, filters
 from pyrogram.types import InlineKeyboardMarkup, InlineKeyboardButton
 
